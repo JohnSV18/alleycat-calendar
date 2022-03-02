21a474b64f4b4d25c310ebc90a3a5540ed09f7c2
John Marcos <johnsaguay@gmail.com>
2022-03-02 10:14:57 -0800

    Header compenent built

---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+function Header() {
+    return (
+        <div className="Header">
+            <h1>Alleycat Calendar</h1>
+            <div className='Header-Subtitle'>The Online Schedule of Upcoming Races.</div>
+        </div>
+    )
+}
+export default Header;
